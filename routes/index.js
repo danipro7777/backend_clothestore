543c109e18c45946b9d65e778955788c9fe4368f
danipro777 <daniel1603@mesoamericana.edu.gt>
2024-09-03 16:26:22 -0600

    first commit subiendo plantilla

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = Router();
+
+// Aqui van los imports
+
+//RUTAS
+
+module.exports = (app) => {
+
+    //AQUI VAN LAS RUTAS
+
+    app.use('/', router);
+
+};
