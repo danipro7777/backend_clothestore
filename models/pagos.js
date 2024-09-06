38a701f609d5c6c29174e4c8c0e1c733d6941b28
danipro777 <daniel1603@mesoamericana.edu.gt>
2024-09-06 16:07:54 -0600

    Tabla de pagos realizada y con crud probado, todo funciona

---
+++ models/pagos.js
@@ -0,0 +1,57 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class pagos extends Model {
+  };
+
+  pagos.init({
+    idPago: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    transferencia: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    banco: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    correlativo: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    tarjetaCredito: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    numeroCredito: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    tarjetaDebito: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    numeroDebito: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    efectivo: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    paypal: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'pagos',
+    timestamps: false // No incluir createdAt y updatedAt
+  });
+
+  return pagos;
+};
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ const { Router } = require('express');
 const router = Router();
 
 // Aqui van los imports
-
-//RUTAS
+const pagosController = require('../controllers/pagosController');
 
 module.exports = (app) => {
 
     //AQUI VAN LAS RUTAS
+    // Pagos
+    router.get('/pagos', pagosController.findAll);
+    router.get('/pagos/:id', pagosController.findById);
+    router.post('/pagos', pagosController.create);
+    router.put('/pagos/:id', pagosController.update);
+    router.delete('/pagos/:id', pagosController.delete);
 
     app.use('/', router);
 
