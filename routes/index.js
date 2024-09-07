8baac340c5ad78e5736377d6be43cda886fb7791
AngelyThC <angelyth321@umes.edu.gt>
2024-09-06 20:27:53 -0600

    Rutas arreglada

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,53 @@
 const { Router } = require('express');
 const router = Router();
+const { authenticateToken } = require('../middlewares/authenticateToken');
 
-// Aqui van los imports
-
-//RUTAS
+// Importa los controladores
+const usuariosController = require('../controllers/usuariosController');
+const pagosController = require('../controllers/pagosController');
+const temporadasController = require('../controllers/temporadasController');
+const logpreguntasController = require('../controllers/logpreguntasController');
 
 module.exports = (app) => {
+    // Ruta para el login
+    router.post('/login', usuariosController.login); // No se le aplica token porque es la ruta de login
 
-    //AQUI VAN LAS RUTAS
+    // Ruta para crear un nuevo usuario
+    router.post('/usuarios', usuariosController.create); // No se le aplica token porque es la ruta de creación de usuario
+
+    // <-------------------- USO DE TOKENS APARTIR DE AQUI --------------------
+    router.use(authenticateToken); // Middleware para verificar el token
+
+    // <-------------------- RUTAS --------------------
+    // Rutas CRUD para usuarios
+    router.get('/usuarios', usuariosController.find); // Obtiene todos los usuarios activos
+    router.get('/usuarios/:id', usuariosController.findById); // Obtiene un usuario por ID
+    router.put('/usuarios/:id', usuariosController.update); // Actualiza un usuario por ID
+    router.delete('/usuarios/:id', usuariosController.delete); // Elimina un usuario por ID
+
+    // Rutas CRUD para pagos
+    router.get('/pagos', pagosController.findAll);
+    router.get('/pagos/:id', pagosController.findById);
+    router.post('/pagos', pagosController.create);
+    router.put('/pagos/:id', pagosController.update);
+    router.delete('/pagos/:id', pagosController.delete);
+
+    router.post('/logout', authenticateToken, usuariosController.logout); // Ruta para cerrar sesión
+
+      //RUTAS CRUD TEMPORADAS
+      router.get('/temporada', temporadasController.find);
+      router.get('/temporada/:id', temporadasController.findById);
+      router.post('/temporada/create', temporadasController.createTemporada);
+      router.put('/temporada/update/:id', temporadasController.updateTemporada);
+      router.delete('/temporada/delete/:id', temporadasController.deleteTemporada);
+  
+      
+      //RUTAS CRUD LOG DE PREGUNTAS
+      router.get('/logpreguntas', logpreguntasController.find);
+      router.get('/logpreguntas/:id', logpreguntasController.findById);
+      router.post('/logpreguntas/create', logpreguntasController.createLogPreguntas);
+      router.put('/logpreguntas/update/:id', logpreguntasController.updateLogPreguntas);
+      router.delete('/logpreguntas/delete/:id', logpreguntasController.deleteLogPreguntas);
 
     app.use('/', router);
-
 };
