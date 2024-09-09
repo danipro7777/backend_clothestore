edd0a22f37faa20a947fa8c676b1edd0abd6b214
PawelReynoso24gt <pawelreynoso@gmail.com>
2024-09-08 20:58:22 -0600

    relacion de modelos para la creacion de detalletemporadas

--- models/productos.js
+++ models/productos.js
@@ -3,7 +3,13 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class productos extends Model {
-  };
+    static associate(models) {
+      // Relación uno a muchos con detalletemporadas
+      productos.hasMany(models.detalletemporadas, {
+        foreignKey: 'idProducto'
+      });
+    }
+  }
 
   productos.init({
     idProducto: {
@@ -16,26 +22,26 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     genero: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     color: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     descripcion: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     estado: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false
     },
   }, {
     sequelize,
-    modelName: 'productos', // nombre del modelo
-    tableName: 'productos', // Nombre de la tabla
-    timestamps: false // No incluir createdAt y updatedAt
+    modelName: 'productos',
+    tableName: 'productos',
+    timestamps: false
   });
 
   return productos;
